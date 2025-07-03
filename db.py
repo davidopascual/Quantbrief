832bff7f98b005eba71f76abba47e5dcd4627831
David Pascual <pascualodavid25@gmail.com>
2025-07-03 10:40:16 -0400

    Style tests pass

--- db.py
+++ db.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
+
 Base = declarative_base()
+
 
 class Summary(Base):
     __tablename__ = 'summaries'
@@ -15,9 +17,11 @@ class Summary(Base):
     sentiment = Column(String, nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow)
 
+
 def init_db(db_path='sqlite:///summarizer.db'):
     engine = create_engine(db_path)
     Base.metadata.create_all(engine)
     return sessionmaker(bind=engine)
+
 
 Session = init_db()
